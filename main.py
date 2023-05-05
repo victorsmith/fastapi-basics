3c0efe7bbfccacce90573966ae18c3b681ccedd9
victorsmith <victor7smith@gmail.com>
2023-05-04 20:47:06 -0600

    setup

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# Order matters if we're going to have multiple paths with the same path operation (beginning of the path)
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
